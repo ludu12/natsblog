eac8a173ca161cb363b73fea80cccbb69831dcdc
ludu12 <dunscombe1@gmail.com>
2018-03-04 15:32:37 -0600

    Added list group highlighting and badge info for blog categories

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -59,7 +59,7 @@ exports = module.exports = function (req, res) {
 
 		var q = keystone.list('Post').paginate({
 			page: req.query.page || 1,
-			perPage: 10,
+			perPage: 5,
 			maxPages: 10,
 			filters: {
 				state: 'published',
